3a42db54c4338b66c1a506b469b7bb43ee0f3c1e
Gordon Young <gjyoung1974@gmail.com>
2019-05-25 08:53:54 -0700

    first commit

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+kubectl apply -f ./all-in-one.yaml
+
+cat <<EOF | kubectl apply -f -
+apiVersion: elasticsearch.k8s.elastic.co/v1alpha1
+kind: Elasticsearch
+metadata:
+  name: quickstart
+spec:
+  version: 7.1.0
+  nodes:
+  - nodeCount: 1
+    config:
+      node.master: true
+      node.data: true
+      node.ingest: true
+EOF
+
+#
+
+cat <<EOF | kubectl apply -f -
+apiVersion: kibana.k8s.elastic.co/v1alpha1
+kind: Kibana
+metadata:
+  name: quickstart
+spec:
+  version: 7.1.0
+  nodeCount: 1
+  elasticsearchRef:
+    name: quickstart
+EOF
+
+PASSWORD=$(kubectl get secret quickstart-elastic-user -o=jsonpath='{.data.elastic}' | base64 --decode)
+
+echo $PASSWORD
+
