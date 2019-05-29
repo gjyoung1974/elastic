07192e54ef4bb29c6f0107b12ebfc0a6c787ff10
Gordon Young <gjyoung1974@gmail.com>
2019-05-28 18:56:18 -0700

    new file:   k8s-fluentbit-es-all-in-one.yaml 	modified:   k8s/auditbeat-kubernetes.yaml 	modified:   k8s/filebeat-daemonset.yaml 	deleted:    k8s/fluentbit/fluent-bit-configmap.yaml 	deleted:    k8s/fluentbit/fluent-bit-ds-minikube.yaml 	deleted:    k8s/fluentbit/fluent-bit-ds.yaml 	deleted:    k8s/fluentbit/fluent-bit-role-binding.yaml 	deleted:    k8s/fluentbit/fluent-bit-role.yaml 	deleted:    k8s/fluentbit/fluent-bit-service-account.yaml 	deleted:    k8s/fluentbit/go.sh 	new file:   k8s/fluentd-daemonset-cloud.yaml 	renamed:    k8s/fluentd-daemonset.yaml -> k8s/fluentd-daemonset-minikube.yaml 	renamed:    k8s/all-in-one-EFK-dev-cluster.yaml -> minikube/all-in-one-EFK-dev-cluster.yaml 	renamed:    k8s/deploy.sh -> minikube/deploy.sh 	renamed:    minikube.sh -> minikube/minikube.sh 	modified:   vms-non-k8s-hosts/filebeat.yml 	modified:   vms-non-k8s-hosts/metricbeat.yml

--- k8s/fluentbit/go.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/fluent-bit-service-account.yaml > fluent-bit-service-account.yaml
-curl https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/fluent-bit-role.yaml > fluent-bit-role.yaml
-curl https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/fluent-bit-role-binding.yaml > fluent-bit-role-binding.yaml
